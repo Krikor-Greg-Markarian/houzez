ae71b527efca5a8e5fcde45262156ff776617e50
Greg <greg_markarian@hotmail.com>
2021-10-21 17:35:14 +0300

    ADD: component

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -7,7 +7,7 @@ function Cities(props) {
       <div className= {cn("w-60 h-60 opacity-100")} style={{ backgroundImage: `url(${props.imageUrl})` }}>
           
         <p className= {cn("text-white text-2xl transform translate-y-20 text-center")}>{props.city}</p>
-        <p className= {cn("text-white text-center transform translate-y-24 text-sm")}>{props.property}</p>
+        <p className= {cn("text-white text-center transform translate-y-20 text-sm")}>{props.property}</p>
         </div>
       
     </div>
