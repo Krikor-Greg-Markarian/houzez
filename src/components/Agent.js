009d537ee138bb6247c47de1eece8c0c8ff12f1a
Greg <greg_markarian@hotmail.com>
2021-10-21 17:38:29 +0300

    ADD: cursor pointer

--- src/components/Agent.js
+++ src/components/Agent.js
@@ -3,10 +3,19 @@ import cn from "classnames";
 
 function Agent(props) {
   return (
-    <div className={cn("p-12 bg-white border-r-2 border-gray-50 hover:transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:shadow-2xl")}>
-
-      <img className={cn("rounded-full")} src={props.imageUrl} alt="" />
-      <p className={cn("text-blue-600 text-center pt-4")}>{props.title}</p>
+    <div
+      className={cn(
+        "p-12 bg-white border-r-2 border-gray-50 hover:transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:shadow-2xl"
+      )}
+    >
+      <img
+        className={cn("rounded-full cursor-pointer")}
+        src={props.imageUrl}
+        alt=""
+      />
+      <p className={cn("text-blue-600 text-center pt-4 cursor-pointer")}>
+        {props.title}
+      </p>
       <p className={cn("font-bold text-center pt-2 pb-4 text-sm")}>
         {props.description}
       </p>
