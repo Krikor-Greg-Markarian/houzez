c2a74abd314c5e9a20b7ba05761473d8607e8bb8
Greg <greg_markarian@hotmail.com>
2021-10-22 17:11:58 +0300

    del: unused section

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -3,13 +3,26 @@ import cn from "classnames";
 
 function Cities(props) {
   return (
-    <div className= {cn("cursor-pointer hover:opacity-75")}>
-      <div className= {cn("w-60 h-60 opacity-100")} style={{ backgroundImage: `url(${props.imageUrl})` }}>
-          
-        <p className= {cn("text-white text-2xl transform translate-y-20 text-center")}>{props.city}</p>
-        <p className= {cn("text-white text-center transform translate-y-20 text-sm")}>{props.property}</p>
-        </div>
-      
+    <div className={cn("cursor-pointer hover:opacity-75")}>
+      <div
+        className={cn("w-60 h-60 opacity-100")}
+        style={{ backgroundImage: `url(${props.imageUrl})` }}
+      >
+        <p
+          className={cn(
+            "text-white text-2xl transform translate-y-20 text-center"
+          )}
+        >
+          {props.city}
+        </p>
+        <p
+          className={cn(
+            "text-white text-center transform translate-y-20 text-sm"
+          )}
+        >
+          {props.property}
+        </p>
+      </div>
     </div>
   );
 }
