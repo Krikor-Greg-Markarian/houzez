ac9aa47bbc4d4b61f5719056005de4404985bc31
Greg <greg_markarian@hotmail.com>
2021-10-25 15:07:00 +0300

    FIX: position of the box button lg

--- src/components/ContemporaryApartment.js
+++ src/components/ContemporaryApartment.js
@@ -5,41 +5,41 @@ import Button from "./Button";
 
 function ContemporaryApartment(props) {
   return (
-    <div className = {cn("container mx-auto pt-80")}>
-    <div className={cn("bg-white w-3/12 relative p-6 rounded transform")}>
-      <div className={cn("grid grid-cols-2")}>
-        <div className={cn("col-span-1")}>
-          <p>{props.title}</p>
+    <div className={cn("container mx-auto pt-60")}>
+      <div className={cn("bg-white w-4/12 relative p-6 rounded")}>
+        <div className={cn("grid grid-cols-2")}>
+          <div className={cn("col-span-1")}>
+            <p>{props.title}</p>
+          </div>
+          <div className={cn("col-span-1")}>
+            <p className={cn("float-right font-bold text-lg")}>{props.price}</p>
+          </div>
         </div>
-        <div className={cn("col-span-1")}>
-          <p className={cn("float-right font-bold text-lg")}>{props.price}</p>
+        <p className={cn("text-gray-500 text-sm pb-2")}>{props.description}</p>
+        <div className={cn("flex")}>
+          <div className={cn("flex-shrink")}>
+            <MdBed className={cn("inline")} />
+            <p className={cn("inline")}>{props.MdBed}</p>
+          </div>
+          <div className={cn("flex-shrink pl-2")}>
+            <MdOutlineWaterDrop className={cn("inline")} />
+            <p className={cn("inline")}>{props.MdOutlineWaterDrop}</p>
+          </div>
+          <div className={cn("flex-shrink pl-2")}>
+            <MdOutlineMap className={cn("inline")} />
+            <p className={cn("inline")}>{props.MdOutlineMap}</p>
+          </div>
+        </div>
+        <p className={cn("font-bold")}>{props.apartment}</p>
+        <div className={cn("absolute bottom-0 right-0")}>
+          <Button
+            className={cn(
+              "bg-blue-400 p-2 text-center text-white rounded-tl rounded-br"
+            )}
+            buttonName={"Details"}
+          />
         </div>
       </div>
-      <p className= {cn("text-gray-500 text-sm pb-2")}>{props.description}</p>
-      <div className={cn("flex")}>
-        <div className={cn("flex-shrink")}>
-          <MdBed className={cn("inline")} />
-          <p className={cn("inline")}>{props.MdBed}</p>
-        </div>
-        <div className={cn("flex-shrink pl-2")}>
-          <MdOutlineWaterDrop className={cn("inline")} />
-          <p className={cn("inline")}>{props.MdOutlineWaterDrop}</p>
-        </div>
-        <div className={cn("flex-shrink pl-2")}>
-          <MdOutlineMap className={cn("inline")} />
-          <p className={cn("inline")}>{props.MdOutlineMap}</p>
-        </div>
-      </div>
-      <p className={cn("font-bold")}>{props.apartment}</p>
-      <div className={cn("absolute bottom-0 right-0")}>
-        <Button
-          className={cn(
-            "bg-blue-400 p-2 text-center text-white rounded-tl-lg rounded-br-lg"
-          )}
-          buttonName={"Details"}
-        />
-      </div>
-    </div>
     </div>
   );
 }
