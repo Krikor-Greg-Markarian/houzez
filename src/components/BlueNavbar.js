6f6c15f74a9a11904adc92b07d809b8abc1fad35
Greg <greg_markarian@hotmail.com>
2021-10-25 10:45:26 +0300

    FIX: bluenavbar pl ml positions

--- src/components/BlueNavbar.js
+++ src/components/BlueNavbar.js
@@ -6,76 +6,90 @@ import Button from "./Button";
 function BlueNavbar(props) {
   return (
     <div className={cn("bg-blue-900 py-2")}>
-      <div className={cn(" flex justify-center items-center ")}>
-        <li
-          className={cn("text-white list-none pl-3 inline hover:text-blue-300")}
-        >
-          <Link href="/">
-            <a href="/">Home</a>
-          </Link>
-        </li>
-        <li
-          className={cn("text-white list-none pl-3 inline hover:text-blue-300")}
-        >
-          <Link href="/">
-            <a href="/">Properties</a>
-          </Link>
-        </li>
-        <li
-          className={cn("text-white list-none pl-3 inline hover:text-blue-300")}
-        >
-          <Link href="property">
-            <a href="/">Property</a>
-          </Link>
-        </li>
-        <li
-          className={cn("text-white list-none pl-3 inline hover:text-blue-300")}
-        >
-          <Link href="/">
-            <a href="/">Realtor</a>
-          </Link>
-        </li>
-        <li
-          className={cn("text-white list-none pl-3 inline hover:text-blue-300")}
-        >
-          <Link href="/">
-            <a href="/">Others</a>
-          </Link>
-        </li>
-        <li
-          className={cn("text-white list-none pl-3 inline hover:text-blue-300")}
-        >
-          <Link href="/">
-            <a href="/">Contact</a>
-          </Link>
-        </li>
+      <div className={cn("flex justify-center")}>
+        <div className={cn(" flex justify-start items-center w-8/12 pl-6")}>
+          <li
+            className={cn(
+              "text-white list-none pl-3 inline hover:text-blue-300"
+            )}
+          >
+            <Link href="/">
+              <a href="/">Home</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-white list-none pl-3 inline hover:text-blue-300"
+            )}
+          >
+            <Link href="/">
+              <a href="/">Properties</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-white list-none pl-3 inline hover:text-blue-300"
+            )}
+          >
+            <Link href="property">
+              <a href="/">Property</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-white list-none pl-3 inline hover:text-blue-300"
+            )}
+          >
+            <Link href="/">
+              <a href="/">Realtor</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-white list-none pl-3 inline hover:text-blue-300"
+            )}
+          >
+            <Link href="/">
+              <a href="/">Others</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-white list-none pl-3 inline hover:text-blue-300"
+            )}
+          >
+            <Link href="/">
+              <a href="/">Contact</a>
+            </Link>
+          </li>
 
-        <div className={cn("flex justify-end w-3/12 ")}>
-          <li
-            className={cn(
-              "text-white list-none mt-2 pl-3 inline hover:text-blue-300"
-            )}
-          >
-            <Link href="/">
-              <a href="">Login</a>
-            </Link>
-          </li>
-          <li
-            className={cn(
-              "text-white  pr-3 mt-2 list-none pl-3 inline hover:text-blue-300"
-            )}
-          >
-            <Link href="/">
-              <a href="/">Register</a>
-            </Link>
-          </li>
-          <button
-            className={cn(
-              "bg-blue-400 py-3 px-4 text-center rounded text-white text-sm font-bold"
-            )}
-          >
-            Create a listing{" "}
-          </button>
+          <div className={cn("flex justify-end w-7/12 mr-44")}>
+            <li
+              className={cn(
+                "text-white list-none mt-2 pl-3 inline hover:text-blue-300"
+              )}
+            >
+              <Link href="/">
+                <a href="">Login</a>
+              </Link>
+            </li>
+            <li
+              className={cn(
+                "text-white  pr-3 mt-2 list-none pl-3 inline hover:text-blue-300"
+              )}
+            >
+              <Link href="/">
+                <a href="/">Register</a>
+              </Link>
+            </li>
+
+            <Button
+              className={cn(
+                "bg-blue-400 py-3 px-4 text-center rounded text-white text-sm font-bold"
+              )}
+              buttonName={"Create a listing"}
+            />
+          </div>
         </div>
       </div>
     </div>
