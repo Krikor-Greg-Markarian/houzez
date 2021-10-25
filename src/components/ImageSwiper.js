ad3efa58faf8aea18783b289a7811d7fcb7db2d6
Greg <greg_markarian@hotmail.com>
2021-10-25 15:00:41 +0300

    ADD: ample apartment section added

--- src/components/ImageSwiper.js
+++ src/components/ImageSwiper.js
@@ -8,6 +8,8 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import cn from "classnames";
+import { Container } from "postcss";
+import ContemporaryApartment from "./ContemporaryApartment";
 
 export default ({ featuredListingsItems }) => {
   return (
@@ -23,22 +25,31 @@ export default ({ featuredListingsItems }) => {
       onSlideChange={() => console.log("slide change")}
     >
       <SwiperSlide>
-        
-        <div className={cn("kitchen")}>
-          
+        <div className={cn("kitchen ")}>
+          <ContemporaryApartment
+            // key={idx}
+            // title={item.title}
+            // price={item.price}
+            // description={item.description}
+            // MdBed={item.MdBed}
+            // MdOutlineWaterDrop={item.MdOutlineWaterDrop}
+            // MdOutlineMap={item.MdOutlineMap}
+            // apartment={item.apartment}
+            title={"Ample Apartment"}
+            price={"$1,900/mo"}
+            description={"3617 Clarington Ave, Los Angeles, CA 90034, USA"}
+            MdBed={"2"}
+            MdOutlineWaterDrop={"1"}
+            MdOutlineMap={"2300 Sq Ft"}
+            apartment={"Apartment"}
+          />
         </div>
       </SwiperSlide>
       <SwiperSlide>
-        
-        <div className={cn("kitchen2")}>
-          
-        </div>
+        <div className={cn("kitchen2")}></div>
       </SwiperSlide>
       <SwiperSlide>
-        
-        <div className={cn("kitchen3")}>
-          
-        </div>
+        <div className={cn("kitchen3")}></div>
       </SwiperSlide>
     </Swiper>
   );
