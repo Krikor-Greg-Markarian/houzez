5eb2ea6865c3e92cf6492e46425e20a2891f5823
Greg <greg_markarian@hotmail.com>
2021-10-25 15:07:11 +0300

    DEL: unused import

--- src/components/ImageSwiper.js
+++ src/components/ImageSwiper.js
@@ -8,7 +8,6 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import cn from "classnames";
-import { Container } from "postcss";
 import ContemporaryApartment from "./ContemporaryApartment";
 
 export default ({ featuredListingsItems }) => {
